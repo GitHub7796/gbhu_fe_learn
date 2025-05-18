3df5aac9963c3397f6ba6fb39523224d2797df3c
ahu <2659813569@qq.com>
2025-05-18 22:09:49 +0800

    add：API

---
+++ src/api/product/spu/index.ts
@@ -0,0 +1,48 @@
+import request from '@/utils/request'
+import type {
+  SkuInfoData,
+  SkuData,
+  SpuData,
+  HasSpuResponseData,
+  AllTradeMark,
+  SpuHasImg,
+  SaleAttrResponseData,
+  HasSaleAttrResponseData,
+} from './type'
+enum API {
+  //  GET /product/1(page)/3(limit)?category3Id=1 获取SPU内容
+  HASSPU_URL = ' /product/',
+  // GET /product/baseTrademark/getTrademarkList 获取全部品牌的数据
+  ALLTRADEMARK_URL = '/product/baseTrademark/getTrademarkList/',
+  // GET /product/spuImageList/14849408842400154 获取某个SPU下的全部的售卖商品的图片数据
+  IMAGE_URL = '/product/spuImageList/',
+  // GET /product/spuSaleAttrList/148494088424001540 获取某一个SPU下全部的已有的销售属性接口地址
+  SPUHASSALEATTR_URL = '/product/spuSaleAttrList/',
+  // GET /product/baseSaleAttrList 获取整个项目全部的销售属性[颜色、版本、尺码]
+  ALLSALEATTR_URL = '/product/baseSaleAttrList',
+  // POST /image/uploadImage 上传图片接口
+
+  // POST /product/updateSpuInfo 修改SPU接口
+  UPDATESPU_URL = '/product/updateSpuInfo',
+  // POST /product/saveSpuInfo 新增SPU接口
+  ADDSPU_URL = '/product/saveSpuInfo',
+  // DELETE /product/deleteSpu/148502877067939840 删除SPU
+  REMOVESPU_URL = '/product/deleteSpu/148502877067939840',
+}
+export const reqHasSpu = (
+  page: number,
+  limit: number,
+  category3Id: number | string,
+) =>
+  request.get<any, HasSpuResponseData>(
+    API.HASSPU_URL + `${page}/${limit}?category3Id=${category3Id}`,
+  )
+
+export const reqAllTradeMark = () =>
+  request.get<any, AllTradeMark>(API.ALLTRADEMARK_URL)
+
+export const reqSpuImageList = (spuId: number) =>
+  request.get(API.IMAGE_URL + spuId)
+
+export const reqSpuHasSaleAttr = (spuId: number) =>
+  request.get(API.SPUHASSALEATTR_URL + spuId)
---
+++ src/api/product/spu/type.ts
@@ -0,0 +1,247 @@
+import { ResponseData } from '@/api/type'
+/**
+ * 获取 SPU 内容
+ *{
+    "data": {
+        "current": 1,
+        "pages": 4,
+        "total": 10,
+        "size": 3,
+        "records": [
+            {
+                "id": 148494088424001538,
+                "category3Id": 1,
+                "tmId": 1,
+                "description": "description10",
+                "spuName": "spu10",
+                "createTime": "2025-05-17T05:34:48.438+00:00",
+                "updateTime": null,
+                "spuImageList": null,
+                "spuPosterList": null,
+                "spuSaleAttrList": null
+            }
+        ]
+    },
+    "code": 200,
+    "message": "成功",
+    "success": true
+}
+ *
+ */
+export interface SpuData {
+  id: number
+  category3Id: number
+  tmId: number
+  description: string
+  spuName: string
+  createTime: string | null
+  updateTime: string | null
+  spuImageList: SpuImg[] | null
+  spuSaleAttrList: SaleAttr[] | null
+}
+/**
+ * 获取全部品牌的数据
+ * {
+    "data": [
+        {
+            "id": 14,
+            "tmName": "Xiaomi SU 7",
+            "logoUrl": "http://139.198.127.41:9000/sph/20240331/img_pic_831703750738.jpg",
+            "createTime": "2025-05-17T05:34:47.794+00:00",
+            "updateTime": null
+        }
+    ],
+    "code": 200,
+    "message": "成功",
+    "success": true
+}
+ */
+export interface Trademark {
+  id: number
+  tmName: string
+  logoUrl: string
+  createTime: string | null
+  updateTime: string | null
+}
+export interface AllTradeMark extends ResponseData {
+  data: Trademark[]
+}
+/**
+ * 获取某个SPU下的全部的售卖商品的图片数据
+ * {
+    "data": [
+        {
+            "id": 148499444382437380,
+            "spuId": 148494088424001540,
+            "imageName": "329a7c4b-c33c-4943-9f7c-4c587cb418cc.png",
+            "imageUrl": "/image/getImage/148499418209980416/329a7c4b-c33c-4943-9f7c-4c587cb418cc.png",
+            "createTime": "2025-05-17T05:56:18.833+00:00",
+            "updateTime": null
+        }
+    ],
+    "code": 200,
+    "message": "成功",
+    "success": true
+}
+ */
+export interface SpuImg {
+  id: number
+  spuId: number
+  imageName: string
+  imageUrl: string
+  createTime: string | null
+  updateTime: string | null
+}
+export interface SpuHasImg extends ResponseData {
+  data: SpuImg[]
+}
+/**
+ * 获取某一个SPU下全部的已有的销售属性接口地址
+ * {
+    "data": [
+        {
+            "id": 148499444382437380,
+            "spuId": 148494088424001540,
+            "baseSaleAttrId": 1,
+            "saleAttrName": "颜色",
+            "createTime": null,
+            "updateTime": null,
+            "spuSaleAttrValueList": [
+                {
+                    "id": 148499444382437380,
+                    "spuId": 148494088424001540,
+                    "spuSaleNameId": 148499444382437380,
+                    "baseSaleAttrId": 1,
+                    "saleAttrName": "颜色",
+                    "saleAttrValueName": "123",
+                    "isChecked": null,
+                    "createTime": "2025-05-17T05:56:18.835+00:00",
+                    "updateTime": null
+                }
+            ]
+        }
+    ],
+    "code": 200,
+    "message": "成功",
+    "success": true
+}
+ */
+export interface SaleAttrValue {
+  id: number
+  spuId: number
+  spuSaleNameId: number
+  baseSaleAttrId: number
+  saleAttrName: string
+  isChecked: null | boolean
+  createTime: string | null
+  updateTime: string | null
+}
+export type SpuSaleAttrValueList = SaleAttrValue[]
+export interface SaleAttr {
+  id: number
+  spuId: number
+  baseSaleAttrId: number
+  saleAttrName: string | number
+  createTime: string | null
+  updateTime: string | null
+}
+export interface SaleAttrResponseData extends ResponseData {
+  data: SaleAttr[]
+}
+/**
+ * 获取整个项目全部的销售属性[颜色、版本、尺码]
+ * {
+    "data": [
+        {
+            "id": 1,
+            "name": "颜色",
+            "createTime": "2025-05-17T05:34:47.806+00:00",
+            "updateTime": null
+        },
+  
+    ],
+    "code": 200,
+    "message": "成功",
+    "success": true
+}
+ */
+export interface HasSaleAttr {
+  id: number
+  name: string
+  createTime: string | null
+  updateTime: string | null
+}
+export interface HasSaleAttrResponseData extends Response {
+  data: HasSaleAttr[]
+}
+/**
+ *  SPU接口 POST对象
+ * {
+	"id": 148494088424001540,
+	"category3Id": 1,
+	"tmId": 1,
+	"spuName": "spu9",
+	"description": "description9",
+	"spuImageList": [
+		{
+			"id": 148499444382437380,
+			"spuId": 148494088424001540,
+			"imageName": "329a7c4b-c33c-4943-9f7c-4c587cb418cc.png",
+			"imageUrl": "/image/getImage/148499418209980416/329a7c4b-c33c-4943-9f7c-4c587cb418cc.png",
+			"createTime": "2025-05-17T05:56:18.833+00:00",
+			"updateTime": null
+		},
+		{
+			"imageName": "7a9426ff-3597-4c2f-9fb8-7de3516f1a01.png",
+			"imageUrl": "/image/getImage/148500844764073984/7a9426ff-3597-4c2f-9fb8-7de3516f1a01.png"
+		}
+	],
+	"spuSaleAttrList": [
+		{
+			"id": 148499444382437380,
+			"spuId": 148494088424001540,
+			"baseSaleAttrId": 1,
+			"saleAttrName": "颜色",
+			"createTime": null,
+			"updateTime": null,
+			"spuSaleAttrValueList": [
+				{
+					"id": 148499444382437380,
+					"spuId": 148494088424001540,
+					"spuSaleNameId": 148499444382437380,
+					"baseSaleAttrId": 1,
+					"saleAttrName": "颜色",
+					"saleAttrValueName": "123",
+					"isChecked": null,
+					"createTime": "2025-05-17T05:56:18.835+00:00",
+					"updateTime": null
+				}
+			]
+		},
+		{
+			"id": 148499500720328700,
+			"spuId": 148494088424001540,
+			"baseSaleAttrId": 3,
+			"saleAttrName": "尺码",
+			"createTime": null,
+			"updateTime": null,
+			"spuSaleAttrValueList": [
+				{
+					"id": 148499500720328700,
+					"spuId": 148494088424001540,
+					"spuSaleNameId": 148499500720328700,
+					"baseSaleAttrId": 3,
+					"saleAttrName": "尺码",
+					"saleAttrValueName": "123",
+					"isChecked": null,
+					"createTime": "2025-05-17T05:56:18.835+00:00",
+					"updateTime": null
+				}
+			]
+		}
+	],
+	"createTime": "2025-05-17T05:34:48.438+00:00",
+	"updateTime": null,
+	"spuPosterList": null
+}
+ */
